54f18d42673e78c33a89b9978481715b5c7af763
Yanzhu Du <yanzhu@cloudcrowd.com>
2011-05-02 15:54:45 -0700

    added sanity check to prevent double-ACKing

--- lib/amqp/header.rb
+++ lib/amqp/header.rb
@@ -5,10 +5,17 @@ module AMQP
     def initialize(mq, header_obj)
       @mq = mq
       @header = header_obj
+      @acked = false
     end
 
     # Acknowledges the receipt of this message with the server.
     def ack
+      # Double-ACKing will cause RabbitMQ 2.4.1 to close channel on us.
+      # Sanity check here to make this method fool-proof
+      return if @acked
+
+      @acked = true
+
       @mq.callback {
         @mq.send Protocol::Basic::Ack.new(:delivery_tag => properties[:delivery_tag])
       }
